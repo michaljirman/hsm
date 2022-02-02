ab6d4789907df7aa13be58fa0e6f16046aa419bc
Michal Jirman <jirman.michal@gmail.com>
2022-02-02 17:35:35 +0000

    wip

--- ra_client/client.go
+++ ra_client/client.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/binary"
 	"encoding/hex"
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/edgelesssys/ego/eclient"
 )
 
 func main() {
@@ -37,11 +31,11 @@ func main() {
 	// the certificate is self-signed and we will verify it using the report instead.
 	tlsConfig := &tls.Config{InsecureSkipVerify: true}
 	certBytes := httpGet(tlsConfig, url+"/cert")
-	reportBytes := httpGet(tlsConfig, url+"/report")
+	//reportBytes := httpGet(tlsConfig, url+"/report")
 
-	if err := verifyReport(reportBytes, certBytes, signer); err != nil {
-		panic(err)
-	}
+	//if err := verifyReport(reportBytes, certBytes, signer); err != nil {
+	//	panic(err)
+	//}
 
 	// Create a TLS config that uses the server certificate as root
 	// CA so that future connections to the server can be verified.
@@ -53,33 +47,33 @@ func main() {
 	fmt.Println("Sent secret over attested TLS channel.")
 }
 
-func verifyReport(reportBytes, certBytes, signer []byte) error {
-	report, err := eclient.VerifyRemoteReport(reportBytes)
-	if err != nil {
-		return err
-	}
-
-	hash := sha256.Sum256(certBytes)
-	if !bytes.Equal(report.Data[:len(hash)], hash[:]) {
-		return errors.New("report data does not match the certificate's hash")
-	}
-
-	// You can either verify the UniqueID or the tuple (SignerID, ProductID, SecurityVersion, Debug).
-
-	if report.SecurityVersion < 2 {
-		return errors.New("invalid security version")
-	}
-	if binary.LittleEndian.Uint16(report.ProductID) != 1234 {
-		return errors.New("invalid product")
-	}
-	if !bytes.Equal(report.SignerID, signer) {
-		return errors.New("invalid signer")
-	}
-
-	// For production, you must also verify that report.Debug == false
-
-	return nil
-}
+//func verifyReport(reportBytes, certBytes, signer []byte) error {
+//	report, err := eclient.VerifyRemoteReport(reportBytes)
+//	if err != nil {
+//		return err
+//	}
+//
+//	hash := sha256.Sum256(certBytes)
+//	if !bytes.Equal(report.Data[:len(hash)], hash[:]) {
+//		return errors.New("report data does not match the certificate's hash")
+//	}
+//
+//	// You can either verify the UniqueID or the tuple (SignerID, ProductID, SecurityVersion, Debug).
+//
+//	if report.SecurityVersion < 2 {
+//		return errors.New("invalid security version")
+//	}
+//	if binary.LittleEndian.Uint16(report.ProductID) != 1234 {
+//		return errors.New("invalid product")
+//	}
+//	if !bytes.Equal(report.SignerID, signer) {
+//		return errors.New("invalid signer")
+//	}
+//
+//	// For production, you must also verify that report.Debug == false
+//
+//	return nil
+//}
 
 func httpGet(tlsConfig *tls.Config, url string) []byte {
 	client := http.Client{Transport: &http.Transport{TLSClientConfig: tlsConfig}}
