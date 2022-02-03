489628b64b5a28a74b6261c89fffb3da3ceaaa37
Michal Jirman <jirman.michal@gmail.com>
2022-02-03 16:14:58 +0000

    wip

--- sdk/client.go
+++ sdk/client.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	`context`
-	`io`
-	`log`
-	`math/rand`
-	`time`
+	"context"
+	"io"
+	"log"
+	"math/rand"
+	"time"
 
-	`google.golang.org/grpc`
+	"github.com/ThalesIgnite/crypto11"
+	"google.golang.org/grpc"
 
-	pb `github.com/michaljirman/hsm/proto`
+	pb "github.com/michaljirman/hsm/proto"
 )
+
+func randomBytes() []byte {
+	result := make([]byte, 32)
+	rand.Read(result)
+	return result
+}
+
+const rsaSize = 2048
 
 func main() {
 	rand.Seed(time.Now().Unix())
@@ -35,6 +44,22 @@ func main() {
 	// first goroutine sends random increasing numbers to stream
 	// and closes it after 10 iterations
 	go func() {
+		ctx, err := crypto11.Configure(&crypto11.Config{
+			Path:              "/opt/nfast/toolkits/pkcs11/libcknfast.so",
+			TokenSerial:       "6D30-03E0-D947",
+			LoginNotSupported: true,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id := randomBytes()
+		_, err = ctx.GenerateRSAKeyPair(id, rsaSize)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("RSA key pair was successfully generated")
+
 		req := pb.Request{Id: 1}
 		if err := stream.Send(&req); err != nil {
 			log.Fatalf("can not send %v", err)
